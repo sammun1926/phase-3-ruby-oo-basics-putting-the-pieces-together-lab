6629ea77f90489f971b3ba68317e5920fedc1d33
sammun1926 <munorusam10@gmail.com>
2023-06-13 07:03:18 +0300

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+class Book
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre
+    def initialize(title)
+        @title = "And Then There Were None"
+    end
 
+    def properties(title, author, page_count, genre)
+        @title
+        @author = "Agatha Christie"
+        @page_count = 272
+        @genre = "Mystery"
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,23 @@
 # Make your shoe class here!
+# Make your shoe class here!# Make your shoe class here!
+class Shoe
+     
+    attr_reader :brand
+    attr_accessor :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def properties(color, size, material, condition)
+        @color = "Red"
+        @size = 9.5
+        @material = "suede"
+        @condition = "tattered"
+    end
+
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+end
